20ffba6352dbb8d9d0635d620bcb80e32931c8f6
DBosDavinci <99069008@mydavinci.nl>
2022-09-23 09:48:52 +0200

    limit

--- challenges/smartphone.py
+++ challenges/smartphone.py
@@ -1,12 +1,23 @@
 iphone = int(input("Hoe duur is de iPhone?\n"))
 samsung = int(input("Hoe duur is de Samsung?\n"))
+teduur = "Het advies is dus geen telefoon te kopen, ze zijn te duur."
+
 
 if samsung > iphone:
     print(f"De Samsung is het duurst, de telefoon kost {samsung} Euro")
     print(f"De iPhone is het goedkoopst, de telefoon kost {iphone} Euro")
-    print(f"Het advies is dus de iPhone te kopen. Deze is namelijk {samsung - iphone} goedkoper dan de Samsung")
+
+    if samsung and iphone < 900:
+        print(f"Het advies is dus de iPhone te kopen. Deze is namelijk {samsung - iphone} goedkoper dan de Samsung")
+    else:
+        print(teduur)
+
 
 elif iphone > samsung:
     print(f"De iPhone is het duurst, de telefoon kost {iphone} Euro")
     print(f"De Samsung is het goedkoopst, de telefoon kost {samsung} Euro")
-    print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de iPhone")
+
+    if samsung and iphone < 900:
+        print(f"Het advies is dus de Samsung te kopen. Deze is namelijk {iphone - samsung} Euro goedkoper dan de iPhone")
+    else:
+        print(teduur)
---
+++ challenges/test.py
@@ -0,0 +1,5 @@
+test = 5
+test2 = 4
+
+if test and test2 > 3:
+    print(True)
